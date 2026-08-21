a4607e77849fdf98640a6884989e71ae2f8131eb
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-08-22 00:30:45 +0600

    feat: update server setup with Express and dotenv integration

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,13 @@
-import express, { Application, Request, Response } from "express";
+import app from "./app";
 
-const app: Application = express();
-const port = 3000; // The port your express server will be running on.
+const bootstrap = () => {
+    try {
+        app.listen(5000, () => {
+            console.log(`Server is running on http://localhost:${5000}`)
+        })
+    } catch (error) {
+        console.error('Failed to start server:', error)
+    }
+}
 
-// Enable URL-encoded form data parsing
-app.use(express.urlencoded({ extended: true }));
-
-// Middleware to parse JSON bodies
-app.use(express.json());
-
-// Basic route
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript + Express!');
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+bootstrap()
