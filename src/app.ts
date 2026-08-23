835a93094e397e323bca36b7ba91c7d97e1f89e7
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-08-23 18:59:14 +0600

    feat: update Prisma setup and enhance root route with specialty creation

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from "express";
+import { prisma } from "./app/lib/prisma";
 
 const app: Application = express()
 
@@ -9,8 +10,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Basic route
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript + Express!');
+app.get('/', async (req: Request, res: Response) => {
+    const specialty = await prisma.specialty.create({
+        data: {
+            title: "Cardiology"
+        }
+    })
+    res.status(201).json({
+        success: true,
+        message: "API is working",
+        data: specialty
+    })
 });
 
 export default app;
