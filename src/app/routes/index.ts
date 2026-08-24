5f2b92f9dc330c5016c7e11fa58884818e3858c8
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-08-24 22:31:11 +0600

    feat(auth): add authentication module with user, session, and account models
    - Added `better-auth` package for authentication management.
    - Created `auth.prisma` schema with User, Session, Account, and Verification models.
    - Introduced enums for User roles and statuses in `enums.prisma`.
    - Implemented authentication logic in `auth.ts` using `better-auth` and Prisma adapter.
    - Set up routes for authentication in `auth.route.ts` and integrated with main router.
    - Created initial structure for `auth.controller.ts` and `auth.service.ts`.

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { SpecialtyRoutes } from "../module/specialty/specialty.route";
+import { AuthRoutes } from "../module/auth/auth.route";
 
 const router = Router()
 
+router.use("/auth", AuthRoutes)
 router.use("/specialties", SpecialtyRoutes)
 export const IndexRoutes = router
