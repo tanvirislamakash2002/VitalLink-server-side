aef3db3eb98685e4bcf1c9f32e5ccf8c1bf95dbf
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-08-24 23:29:23 +0600

    feat(auth): implement registerPatient function for patient registration

---
+++ src/app/module/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { auth } from "../../lib/auth";
+
+interface IRegisterPatientPayload {
+    name: string;
+    email: string;
+    password: string;
+}
+const registerPatient = async (payload: IRegisterPatientPayload) => {
+    const { name, email, password } = payload
+
+    const data = await auth.api.signUpEmail({
+        body: {
+            name,
+            email,
+            password,
+            // default values set in the auth.ts
+            // needPasswordChange: false,
+            // role: "PATIENT"
+        }
+    })
+
+    if (!data.user) {
+        throw new Error("Failed to register patient")
+    }
+}
