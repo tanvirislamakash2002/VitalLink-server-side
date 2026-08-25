9314b25585d1a77ffbaac63863897eb51e87373d
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-08-25 19:10:30 +0600

    feat(middleware): add notFound middleware for handling 404 errors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import { prisma } from "./app/lib/prisma";
 import { IndexRoutes } from "./app/routes";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
+import { notFound } from "./app/middleware/notFound";
 
 const app: Application = express()
 
@@ -28,5 +29,6 @@ app.get('/', async (req: Request, res: Response) => {
 });
 
 app.use(globalErrorHandler)
+app.use(notFound)
 
 export default app;
