0fa5e11e64082f070a38904347a72800feda87a2
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-08-26 14:29:09 +0600

    feat(doctor): implement Doctor module with routes, controller, service, and update interface

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { SpecialtyRoutes } from "../module/specialty/specialty.route";
 import { AuthRoutes } from "../module/auth/auth.route";
 import { UserRoutes } from "../module/user/user.route";
+import { DoctorRoutes } from "../module/doctor/doctor.route";
 
 const router = Router()
 
 router.use("/auth", AuthRoutes)
 router.use("/specialties", SpecialtyRoutes)
-router.use("/doctors", UserRoutes)
+router.use("/users", UserRoutes)
+router.use("/doctors", DoctorRoutes)
 export const IndexRoutes = router
