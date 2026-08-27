9ca13b92a63280b952dd7144c2c799c4bbb0c0c3
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-08-27 23:21:17 +0600

    feat(jwt): implement verifyToken function for JWT verification with error handling

--- src/app/utils/jwt.ts
+++ src/app/utils/jwt.ts
@@ -1,6 +1,23 @@
+/* eslint-disable no-useless-catch */
 import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
 
 const createToken = (payload: JwtPayload, secret: string, { expiresIn }: SignOptions) => {
     const token = jwt.sign(payload, secret, { expiresIn })
     return token;
+}
+
+const verifyToken = (token: string, secret: string) => {
+    try {
+        const decoded = jwt.verify(token, secret) as JwtPayload
+        return {
+            success: true,
+            data: decoded
+        }
+    } catch (error: any) {
+        return {
+            success: false,
+            message: error.message,
+            error
+        }
+    }
 }
