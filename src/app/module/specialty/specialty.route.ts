e0c4a0e8fc4df345eb48311d9ea4be214eea53e6
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-08-31 00:12:17 +0600

    feat: initialize express application with cookie-parser and add protected specialty routes

--- src/app/module/specialty/specialty.route.ts
+++ src/app/module/specialty/specialty.route.ts
@@ -1,10 +1,37 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { SpecialtyController } from "./specialty.controller";
+import { CookieUtils } from "../../utils/cookie";
+import AppError from "../../errorHelpers/AppError";
+import status from "http-status";
+import { jwtUtils } from "../../utils/jwt";
+import { envVars } from "../../../config/env";
 
 const router = Router()
 
 router.post('/', SpecialtyController.createSpecialty)
-router.get('/', SpecialtyController.getAllSpecialties)
+router.get('/', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const accessToken = CookieUtils.getCookie(req, 'accessToken')
+
+        if (!accessToken) {
+            throw new AppError(status.UNAUTHORIZED, "Unauthorized access! No acceess token provided.")
+        }
+
+        const verifiedToken = jwtUtils.verifyToken(accessToken, envVars.ACCESS_TOKEN_SECRET)
+
+        if (!verifiedToken.success) {
+            throw new AppError(status.UNAUTHORIZED, "Unauthorized access! Invalid access token.")
+        }
+
+        if (verifiedToken.data!.role !== "ADMIN") {
+            throw new AppError(status.FORBIDDEN, "Forbidden access! You do not have permission to access this resource.")
+        }
+        next()
+
+    } catch (error: any) {
+        next(error);
+    }
+}, SpecialtyController.getAllSpecialties)
 router.delete('/:id', SpecialtyController.deleteSpecialty)
 
 export const SpecialtyRoutes = router
