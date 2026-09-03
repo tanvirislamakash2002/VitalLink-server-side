b3350a2bf21efcd0ffa06f7e714881a4e1790340
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-09-03 21:33:31 +0600

    feat: implement authentication service and controller logic for user management

--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import status from "http-status";
 import { UserStatus } from "../../../generated/prisma/enums";
 import { auth } from "../../lib/auth";
@@ -390,8 +391,38 @@ const resetPassword = async (email: string, otp: string, newPassword: string) =>
     })
 }
 
-const googleLoginSuccess = async () => {
+const googleLoginSuccess = async (session: Record<string, any>) => {
+    const isPatientExists = await prisma.patient.findUnique({
+        where: {
+            userId: session.user.id,
+        }
+    })
 
+    if (!isPatientExists) {
+        await prisma.patient.create({
+            data: {
+                userId: session.user.id,
+                name: session.user.name,
+                email: session.user.email
+            }
+        })
+    }
+
+    const accessToken = tokenUtils.getAccessToken({
+        userId: session.user.id,
+        role: session.user.role,
+        name: session.user.name
+    })
+
+    const refreshToken = tokenUtils.getRefreshToken({
+        userId: session.user.id,
+        role: session.user.role,
+        name: session.user.name
+    })
+    return {
+        accessToken,
+        refreshToken
+    }
 }
 
 export const AuthService = {
