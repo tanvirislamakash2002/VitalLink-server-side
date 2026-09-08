71e0464e4d5de1b09804a0c5a5d663e92e77ec0e
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-09-08 23:41:11 +0600

    feat: implement Stripe webhook handler for processing payment events

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import path from "path";
 import cors from "cors";
 import { envVars } from "./config/env";
 import qs from "qs"
+import { PaymentController } from "./app/module/payment/payment.controller";
 
 const app: Application = express()
 app.set("query parser", (str: string) => qs.parse(str))
@@ -16,11 +17,7 @@ app.set("query parser", (str: string) => qs.parse(str))
 app.set("view engine", "ejs")
 app.set("views", path.resolve(process.cwd(), `src/app/templates`))
 
-app.post("/webhook",express.raw({type: "application/json"}), async (req: Request, res: Response) => {
-    console.log("Webhook received:", req.body);
-
-    res.status(200).json({ received: true });
-})
+app.post("/webhook", express.raw({ type: "application/json" }), PaymentController.handleStripeWebhookEvent)
 
 app.use(cors({
     origin: [envVars.FRONTEND_URL, envVars.BETTER_AUTH_URL, "http://localhost:3000", "http://localhost:5000"],
---
+++ src/app/module/payment/payment.controller.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response } from "express";
+import { envVars } from "../../../config/env";
+import { catchAsync } from "../../shared/catchAsync";
+import status from "http-status";
+import { stripe } from "../../../config/stripe.config";
+import { PaymentService } from "./payment.service";
+import { sendResponse } from "../../shared/sendResponse";
+
+const handleStripeWebhookEvent = catchAsync(async (req: Request, res: Response) => {
+    const signature = req.headers['stripe-signature'] as string
+    const webhookSecret = envVars.STRIPE.STRIPE_WEBHOOK_SECRET
+
+    if (!signature || !webhookSecret) {
+        console.error("Missing Stripe signature or webhook secret")
+        return res.status(status.BAD_REQUEST).json({ message: "Missing Stripe signature or webhook secret" })
+    }
+
+    let event;
+
+    try {
+        event = stripe.webhooks.constructEvent(req.body, signature, webhookSecret)
+    } catch (error: any) {
+        return res.status(status.BAD_REQUEST).json({ message: "Error processing Stripe webhook" })
+    }
+
+    try {
+        const result = await PaymentService.handlerStripeWebhookEvent(event)
+
+        sendResponse(res, {
+            httpStatusCode: status.OK,
+            success: true,
+            message: "Stripe webhook event processed successfully",
+            data: result
+        })
+    } catch (error) {
+        console.error("Error handling Stripe webhook event:", error)
+        sendResponse(res, {
+            httpStatusCode: status.INTERNAL_SERVER_ERROR,
+            success: false,
+            message: "Error handling Stripe webhook event"
+        })
+    }
+})
+
+export const PaymentController = {
+    handleStripeWebhookEvent
+}
