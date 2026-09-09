aefaaa933d1fa528a6b5b32eea06a4ceeb644969
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-09-09 14:19:36 +0600

    feat: add seedSuperAdmin function and update server bootstrap process

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -82,6 +82,14 @@ export const auth = betterAuth({
                             email,
                         }
                     })
+                    if (!user) {
+                        console.error(`User with email ${email} not found. Cannot send verification OTP.`)
+                        return;
+                    }
+                    if (user && user.role === Role.SUPER_ADMIN) {
+                        console.log(`User with email ${email} is a super admin. Skipping sending verification OTP`);
+                        return
+                    }
                     if (user && !user.emailVerified) {
                         sendEmail({
                             to: email,
--- src/config/env.ts
+++ src/config/env.ts
@@ -35,7 +35,10 @@ interface EnvConfig {
     STRIPE: {
         STRIPE_SECRET_KEY: string;
         STRIPE_WEBHOOK_SECRET: string;
-    }
+    },
+    SUPER_ADMIN_EMAIL: string;
+    SUPER_ADMIN_PASSWORD: string;
+
 }
 
 const loadEnvVariables = (): EnvConfig => {
@@ -66,6 +69,8 @@ const loadEnvVariables = (): EnvConfig => {
         "CLOUDINARY_API_SECRET",
         "STRIPE_SECRET_KEY",
         "STRIPE_WEBHOOK_SECRET",
+        "SUPER_ADMIN_EMAIL",
+        "SUPER_ADMIN_PASSWORD",
     ]
 
     requireEnvVariable.forEach((variable) => {
@@ -105,7 +110,9 @@ const loadEnvVariables = (): EnvConfig => {
         STRIPE: {
             STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY as string,
             STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET as string,
-        }
+        },
+        SUPER_ADMIN_EMAIL: process.env.SUPER_ADMIN_EMAIL as string,
+        SUPER_ADMIN_PASSWORD: process.env.SUPER_ADMIN_PASSWORD as string,
     }
 }
 
