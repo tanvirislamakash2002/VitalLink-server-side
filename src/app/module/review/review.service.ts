a690a710810deea3ae8b2279ad24928a4fb1de1d
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-09-11 14:02:49 +0600

    feat: enhance giveReview function to update doctor's average rating after review creation

--- src/app/module/review/review.service.ts
+++ src/app/module/review/review.service.ts
@@ -5,7 +5,62 @@ import { IRequestUser } from "../../interfaces/requestUser.interface";
 import { prisma } from "../../lib/prisma";
 import { ICreateReviewPayload, IUpdateReviewPayload } from "./review.interface";
 
-const giveReview = async (user : IRequestUser, payload : ICreateReviewPayload) => {
+const giveReview = async (user: IRequestUser, payload: ICreateReviewPayload) => {
+    const patientData = await prisma.patient.findUniqueOrThrow({
+        where: {
+            email: user.email
+        }
+    })
+    const appointmentData = await prisma.appointment.findUniqueOrThrow({
+        where: {
+            id: payload.appointmentId
+        }
+    })
+    if (appointmentData.paymentStatus !== PaymentStatus.PAID) {
+        throw new AppError(status.BAD_REQUEST, "You can only review after payment is done")
+    }
+    if (appointmentData.patientId !== patientData.id) {
+        throw new AppError(status.BAD_REQUEST, "You can only review for your own appointments")
+    }
+
+    const isReviewed = await prisma.review.findFirst({
+        where: {
+            appointmentId: payload.appointmentId
+        }
+    })
+
+    if (isReviewed) {
+        throw new AppError(status.BAD_REQUEST, "You have already reviewed for this appointment. You can update your review instead.")
+    }
+    const result = await prisma.$transaction(async (tx) => {
+        const review = await tx.review.create({
+            data: {
+                ...payload,
+                patientId: appointmentData.patientId,
+                doctorId: appointmentData.doctorId
+            }
+        })
+
+        const averageRating = await tx.review.aggregate({
+            where: {
+                doctorId: appointmentData.doctorId
+            },
+            _avg: {
+                rating: true
+            }
+        }) || { _avg: { rating: 0 } }
+
+        await tx.doctor.update({
+            where: {
+                id: appointmentData.doctorId
+            },
+            data: {
+                averageRating: averageRating._avg.rating as number
+            }
+        })
+        return review
+    })
+    return result
 };
 
 const getAllReviews = async (
