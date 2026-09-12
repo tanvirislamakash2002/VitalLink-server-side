ac7c66a30cd364028e07b12c5b1f708e31390f69
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-09-12 22:48:14 +0600

    feat: refactor dashboard stats data retrieval logic for improved readability and structure

--- src/app/module/stats/stats.service.ts
+++ src/app/module/stats/stats.service.ts
@@ -4,8 +4,26 @@ import AppError from "../../errorHelpers/AppError";
 import { IRequestUser } from "../../interfaces/requestUser.interface";
 import { prisma } from "../../lib/prisma";
 
-const getDashboardStatsData = async (user : IRequestUser) => {
+const getDashboardStatsData = async (user: IRequestUser) => {
+    let statsData;
 
+    switch (user.role) {
+        case Role.SUPER_ADMIN:
+            statsData = getSuperAdminStatsData();
+            break;
+        case Role.ADMIN:
+            statsData = getAdminStatsData();
+            break;
+        case Role.DOCTOR:
+            statsData = getDoctorStatsData();
+            break;
+        case Role.PATIENT:
+            statsData = getPatientStatsData();
+            break;
+        default:
+            throw new AppError(status.BAD_REQUEST, "Invalid user role")
+    }
+    return statsData
 }
 
 const getSuperAdminStatsData = async () => {
@@ -16,20 +34,20 @@ const getAdminStatsData = async () => {
 
 }
 
-const getDoctorStatsData = async (user : IRequestUser) => {
+const getDoctorStatsData = async (user: IRequestUser) => {
 
 }
 
-const getPatientStatsData = async (user : IRequestUser) => {
-    
+const getPatientStatsData = async (user: IRequestUser) => {
+
 }
 
 const getPieChartData = async () => {
-    
+
 }
 
 const getBarChartData = async () => {
-    
+
 }
 
 
