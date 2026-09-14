f35142ff3671b379c08d4dfeb7db93d70a16ffea
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-09-14 19:24:14 +0600

    feat: refactor server initialization and enhance shutdown handling with process signals

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,65 @@
+import { Server } from "http";
 import app from "./app";
 import { seedSuperAdmin } from "./app/utils/seed";
 import { envVars } from "./config/env";
 
-const bootstrap = async() => {
+let server: Server;
+
+const bootstrap = async () => {
     try {
         await seedSuperAdmin()
-        app.listen(envVars.PORT, () => {
+        server = app.listen(envVars.PORT, () => {
             console.log(`Server is running on http://localhost:${envVars.PORT}`)
         })
     } catch (error) {
         console.error('Failed to start server:', error)
     }
 }
+
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal received. Shutting down server...")
+
+    if (server) {
+        server.close(() => {
+            console.log("Server closed gracefully.")
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+
+})
+
+process.on("SIGINT", () => {
+    console.log("SIGINT signal received. Shutting down server...")
+
+    if (server) {
+        server.close(() => {
+            console.log("Server closed gracefully")
+            process.exit(1)
+        })
+    }
+})
+
+process.on("uncaughtException", (error) => {
+    console.log("Uncaught Exception Detected... Shutting down server", error)
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
+
+process.on("unhandledRejection", (error) => {
+    console.log("Unhandled Rejection Detected... Shutting down server", error)
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+})
 
 bootstrap()
